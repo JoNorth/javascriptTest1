08a5224bb0d9b7d47d0639d40f31f27ae63b1897
JoNorth <38085796+JoNorth@users.noreply.github.com>
2025-02-09 00:18:09 -0800

    Update circle color and fill color in map implementation

--- JavaScriptTest_2015_02_08.js
+++ JavaScriptTest_2015_02_08.js
@@ -12,8 +12,8 @@
 
     // Add a circle
     var circle = L.circle([51.508, -0.11], {
-        color: 'red',
-        fillColor: '#f03',
+        color: 'green',
+        fillColor: '#f43',
         fillOpacity: 0.5,
         radius: 500
     }).addTo(map);
